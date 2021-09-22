b667f6cb1b5fddfcfa30c78f4da3e6a22a33e7a9
ravelinejunior <junior.raveline@gmail.com>
2021-09-21 21:45:15 -0300

    Animation to recyclerView

--- app/src/main/java/br/com/raveline/todo2021/presentation/ui/fragment/TodoListFragment.kt
+++ app/src/main/java/br/com/raveline/todo2021/presentation/ui/fragment/TodoListFragment.kt
@@ -4,6 +4,7 @@ import android.app.AlertDialog
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import android.view.animation.OvershootInterpolator
 
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
@@ -21,6 +22,8 @@ import br.com.raveline.todo2021.presentation.viewmodel.ToDoViewModel
 import br.com.raveline.todo2021.presentation.viewmodel.viewmodel_factory.ToDoViewModelFactory
 import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import jp.wasabeef.recyclerview.animators.LandingAnimator
+import jp.wasabeef.recyclerview.animators.SlideInUpAnimator
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -111,6 +114,10 @@ class TodoListFragment : Fragment() {
             layoutManager = LinearLayoutManager(requireContext())
             setHasFixedSize(true)
             setItemViewCacheSize(25)
+            itemAnimator = LandingAnimator(OvershootInterpolator(1f)).apply {
+                addDuration = 300
+                moveDuration = 500
+            }
             adapter = mAdapter
         }
 
