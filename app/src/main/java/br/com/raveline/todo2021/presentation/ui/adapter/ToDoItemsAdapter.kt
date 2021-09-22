44280402389d7bd4e01fa2533a202910f0a289d6
ravelinejunior <junior.raveline@gmail.com>
2021-09-21 21:13:37 -0300

    Binding adapter for home adapter

--- app/src/main/java/br/com/raveline/todo2021/presentation/ui/adapter/ToDoItemsAdapter.kt
+++ app/src/main/java/br/com/raveline/todo2021/presentation/ui/adapter/ToDoItemsAdapter.kt
@@ -2,21 +2,15 @@ package br.com.raveline.todo2021.presentation.ui.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
-import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
-import br.com.raveline.todo2021.R
 import br.com.raveline.todo2021.data.db.entity.ToDoItemEntity
-import br.com.raveline.todo2021.data.model.Priority
 import br.com.raveline.todo2021.databinding.ItemAdapterTodoBinding
-import br.com.raveline.todo2021.presentation.ui.fragment.TodoListFragmentDirections
 import br.com.raveline.todo2021.utils.ToDoDiffUtils
 
 class ToDoItemsAdapter : RecyclerView.Adapter<ToDoItemsAdapter.MyViewHolder>() {
 
     private var toDoItemsList = emptyList<ToDoItemEntity>()
-
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val mBinding =
@@ -27,12 +21,6 @@ class ToDoItemsAdapter : RecyclerView.Adapter<ToDoItemsAdapter.MyViewHolder>() {
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val toDoItem = toDoItemsList[position]
         holder.bind(toDoItem)
-
-        holder.itemView.setOnClickListener {
-            val action = TodoListFragmentDirections.actionTodoListFragmentToUpdateFragment(toDoItem)
-            holder.itemView.findNavController().navigate(action)
-
-        }
     }
 
     override fun getItemCount(): Int = toDoItemsList.size
@@ -41,40 +29,9 @@ class ToDoItemsAdapter : RecyclerView.Adapter<ToDoItemsAdapter.MyViewHolder>() {
         RecyclerView.ViewHolder(binding.root) {
 
         fun bind(toDoItem: ToDoItemEntity) {
-            binding.textViewTitle.text = toDoItem.title
-            binding.textViewDescription.text = toDoItem.description
-            getPriority(toDoItem)
-        }
+            binding.toDoData = toDoItem
+            binding.executePendingBindings()
 
-        private fun getPriority(toDoItem: ToDoItemEntity) {
-            when (toDoItem.priority) {
-                Priority.LOW -> {
-                    binding.cardViewPriority.setCardBackgroundColor(
-                        ContextCompat.getColor(
-                            binding.cardViewPriority.context,
-                            R.color.lightGreen
-                        )
-                    )
-                }
-
-                Priority.MEDIUM -> {
-                    binding.cardViewPriority.setCardBackgroundColor(
-                        ContextCompat.getColor(
-                            binding.cardViewPriority.context,
-                            R.color.yellow
-                        )
-                    )
-                }
-
-                Priority.HIGH -> {
-                    binding.cardViewPriority.setCardBackgroundColor(
-                        ContextCompat.getColor(
-                            binding.cardViewPriority.context,
-                            R.color.red
-                        )
-                    )
-                }
-            }
         }
 
     }
--- app/src/main/java/br/com/raveline/todo2021/presentation/ui/binding/BindingAdapter.kt
+++ app/src/main/java/br/com/raveline/todo2021/presentation/ui/binding/BindingAdapter.kt
@@ -1,9 +1,17 @@
 package br.com.raveline.todo2021.presentation.ui.binding
 
 import android.view.View
+import android.widget.Spinner
+import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import androidx.lifecycle.LiveData
+import androidx.navigation.findNavController
+import br.com.raveline.todo2021.R
 import br.com.raveline.todo2021.data.db.entity.ToDoItemEntity
+import br.com.raveline.todo2021.data.model.Priority
+import br.com.raveline.todo2021.presentation.ui.fragment.TodoListFragmentDirections
 
 class BindingAdapter {
     companion object {
@@ -14,6 +22,59 @@ class BindingAdapter {
             when (emptyDatabase.value?.isEmpty()) {
                 true -> view.visibility = View.VISIBLE
                 false -> view.visibility = View.GONE
+            }
+        }
+
+        @BindingAdapter("android:parsePriorityToInt")
+        @JvmStatic
+        fun parsePriorityToInt(view: Spinner, priority: Priority) {
+            when (priority) {
+                Priority.HIGH -> view.setSelection(1)
+                Priority.MEDIUM -> view.setSelection(2)
+                Priority.LOW -> view.setSelection(3)
+            }
+        }
+
+        @BindingAdapter("android:parsePriorityColor")
+        @JvmStatic
+        fun parsePriorityColor(cardViewPriority: CardView, value: Priority) {
+
+            when (value) {
+                Priority.LOW -> {
+                    cardViewPriority.setCardBackgroundColor(
+                        ContextCompat.getColor(
+                            cardViewPriority.context,
+                            R.color.lightGreen
+                        )
+                    )
+                }
+
+                Priority.MEDIUM -> {
+                    cardViewPriority.setCardBackgroundColor(
+                        ContextCompat.getColor(
+                            cardViewPriority.context,
+                            R.color.yellow
+                        )
+                    )
+                }
+
+                Priority.HIGH -> {
+                    cardViewPriority.setCardBackgroundColor(
+                        ContextCompat.getColor(
+                            cardViewPriority.context,
+                            R.color.red
+                        )
+                    )
+                }
+            }
+        }
+
+        @BindingAdapter("sendDataToUpdateFragment")
+        @JvmStatic
+        fun sendDataToUpdateFragment(view:ConstraintLayout,currentItem:ToDoItemEntity){
+            view.setOnClickListener {
+                val action = TodoListFragmentDirections.actionTodoListFragmentToUpdateFragment(currentItem)
+                view.findNavController().navigate(action)
             }
         }
     }
